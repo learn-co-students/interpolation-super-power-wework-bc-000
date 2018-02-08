61bbbffae5da878e77fa295c4a623c57ed2fe5ed
Danni Baxter <dbaxter@wework.com>
2018-02-08 22:56:03 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  result = ''
+  colors.each do |element|
+    result << element[0].upcase+': '+element+', '
+  end
+  
+  result2 = result.slice(0..-3)
+  puts result2
+  result2
+end
