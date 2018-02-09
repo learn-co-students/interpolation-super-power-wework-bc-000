1e136c9817c1f098c322ac5a4d73a4cbf03a7a48
Andrew Cooper <andrew.cooper@wework.com>
2018-02-09 20:54:30 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,8 @@
-# Write your #display_rainbow method here
+def display_rainbow(colors)
+  puts display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+end
+
+display_rainbow
+
+
+   
