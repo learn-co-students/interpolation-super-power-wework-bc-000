9a05aac2cfd2d0b3a4c858b553eebf42f97f229e
afox@wework.com <afox@wework.com>
2018-02-17 18:54:12 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,9 @@
 # Write your #display_rainbow method here
+
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+def display_rainbow(arg)
+  puts "R: #{arg[0]}, O: #{arg[1]}, Y: #{arg[2]}, G: #{arg[3]}, B: #{arg[4]}, I: #{arg[5]}, V: #{arg[6]}\n"
+end
+
+display_rainbow(colors)
